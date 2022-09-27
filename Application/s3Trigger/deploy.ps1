1902c6c3d833d33313dd08a3e8e7c03e9c8f8028
Sanjay Gulati <sagulati@amazon.com>
2022-09-27 14:14:55 -0500

    adding local build steps.

---
+++ Application/s3Trigger/deploy.ps1
@@ -0,0 +1,5 @@
+﻿docker build . -t net-7-native-builder
+docker run --volume C:\source\repos\dotnet-serverless-imagerecognition\Application\s3Trigger\:/workingdir --name net7-native-build-container -i net-7-native-builder dotnet publish /workingdir/s3Trigger.csproj -r linux-x64 -c Release
+rm s3Trigger.zip
+Compress-Archive .\bin\Release\net7.0\linux-x64\publish\* s3Trigger.zip
+docker rm net7-native-build-container
