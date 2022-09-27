440b33ea709008c68bede595f6dd6421a5fea174
Sanjay Gulati <sagulati@amazon.com>
2022-09-27 15:36:28 -0500

    only zipping bootstrap file.

--- Application/s3Trigger/deploy.ps1
+++ Application/s3Trigger/deploy.ps1
@@ -1,5 +1,5 @@
 ﻿docker build . -t net-7-native-builder
-docker run --volume C:\source\repos\dotnet-serverless-imagerecognition\Application\s3Trigger\:/workingdir --name net7-native-build-container -i net-7-native-builder dotnet publish /workingdir/s3Trigger.csproj -r linux-x64 -c Release --output /workingdir/publish
+docker run --volume C:\source\repos\dotnet-serverless-imagerecognition\Application\s3Trigger\:/workingdir --name net7-native-build-container -i net-7-native-builder dotnet publish /workingdir/s3Trigger.csproj -r linux-x64 -c Release --self-contained --output /workingdir/publish
 rm s3Trigger.zip
 Compress-Archive /publish/* s3Trigger.zip
 docker rm net7-native-build-container
