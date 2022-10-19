bcaadc8b45d227739baf9c884673ecd72dfa7978
Sanjay Gulati <sagulati@amazon.com>
2022-10-19 16:20:39 -0500

    first pass for OAuthProxy for cognito.

---
+++ Application/API/OAuth/metadata/CustomJsonSerializerContext.cs
@@ -0,0 +1,19 @@
+﻿using Amazon.Lambda.APIGatewayEvents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace metadata
+{
+    [JsonSerializable(typeof(APIGatewayHttpApiV2ProxyRequest))]
+    [JsonSerializable(typeof(APIGatewayHttpApiV2ProxyResponse))]
+    [JsonSerializable(typeof(OpenIdConfiguration))]
+    [JsonSerializable(typeof(List<string>))]
+    [JsonSerializable(typeof(string))]
+    public partial class CustomJsonSerializerContext : JsonSerializerContext
+    {
+    }
+}
---
+++ Application/API/OAuth/metadata/OpenIdConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metadata
+{
+    public class OpenIdConfiguration
+    {
+        public string authorization_endpoint { get; set; }
+        public List<string> id_token_signing_alg_values_supported { get; set; }
+        public string issuer { get; set; }
+        public string jwks_uri { get; set; }
+        public List<string> response_types_supported { get; set; }
+        public List<string> scopes_supported { get; set; }
+        public List<string> subject_types_supported { get; set; }
+        public string token_endpoint { get; set; }
+        public List<string> token_endpoint_auth_methods_supported { get; set; }
+        public string userinfo_endpoint { get; set; }
+    }
+}
